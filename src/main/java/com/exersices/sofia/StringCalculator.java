a1f474f85e27e5ab788355fe0704f8c6675964cc
Sofia <sofiatheodo88@gmail.com>
2017-09-18 23:19:25 +0100

    1st commit - line separator

--- src/main/java/com/exersices/sofia/StringCalculator.java
+++ src/main/java/com/exersices/sofia/StringCalculator.java
@@ -3,42 +3,42 @@ package com.exersices.sofia;
 public class StringCalculator {
 
 
-    public int add(String inputValue) {
+    public int add(String inputValue)
+    {
 
-        if (inputValue.equals("")) {
+        if (inputValue.isEmpty())
+        {
             return 0;
 
-        } else if (inputValue.contains(",")) {
+        }
+        else if (!inputValue.contains("\\d"))
+        {
 
             int total = 0;
 
-            String[] values = inputValue.split(",");
+            String[] values = inputValue.split(",|\n");
 
             for (String value : values)
 
             {
 
-                if (value.contains("\\n")) {
-                    String[] values2 = value.split(System.getProperty("line.separator"));
                     for (String value2 : values2) {
-                        int number = Integer.parseInt(value2);
-                        total += number;
+                        total+=Integer.parseInt(number);
                     }
 
-                }
+            }
 
                 int number = Integer.parseInt(value);
                 total += number;
-            }
+        }
 
             return total;
 
-        } else if (!inputValue.contains(",")) {
-            return Integer.parseInt(inputValue);
-        } else {
-
-            return -1;
-        }
+    }
+    else
+    {
+        return -1;
+    }
     }
 }
 
